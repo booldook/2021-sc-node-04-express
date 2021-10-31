1b51ad2f6a799244258945d81b250b536a285d69
booldook <booldook@gmail.com>
2021-10-31 16:50:23 +0900

    app.js

--- app.js
+++ app.js
@@ -1,59 +1,29 @@
 const express = require("express");
-const axios = require("axios");
 const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`http://127.0.0.1:${port}`));
+require("./modules/server-init")(app, 3500);
 
 app.use("/", express.static("./public"));
 
-app.get("/hello", (req, res) => {
-  // const name = req.query.name;
-  const { name } = req.query;
-  res.status(200).send(`
-  <!doctype html>
-  <html lang="ko">
-    <head>
-      <meta charset="utf-8" />
-      <title>Hello, ${name}</title>
-    </head>
-    <body>
-      <h1>Hello, ${name}</h1>
-    </body>
-  </html>`);
+app.get("/", (req, res) => {
+  res.send("Root");
 });
 
-app.get("/users", async (req, res) => {
-  try {
-    const name = req.query.name;
-    const usersURL = "https://jsonplaceholder.typicode.com/users";
-    const reqUrl = name ? usersURL + "?username=" + name : usersURL;
-    const { data: users } = await axios.get(reqUrl);
-    // res.json(users);
-    let html = `
-    <!doctype html>
-    <html lang="ko">
-      <head>
-        <meta charset="utf-8" />
-        <title>Hello, ${name}</title>
-      </head>
-      <body>
-        <table border="1">`;
-    for (let v of users) {
-      html += `
-        <tr>
-          <td>${v.id}</td>
-          <td>${v.name}</td>
-          <td>${v.username}</td>
-          <td>${v.email}</td>
-          <td>${v.phone}</td>
-        </tr>`;
-    }
-    html += `
-        </table>
-      </body>
-    </html>`;
-    res.status(200).send(html);
-  } catch (err) {
-    console.log(err);
-  }
+app.get("/login", (req, res) => {
+  res.send("GET");
 });
+
+app.post("/login", (req, res) => {
+  res.send("POST");
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>File Not Found</h1>");
+});
+
+/* app.get()
+app.post()
+app.put()
+app.delete()
+app.use() */
+
+// CRUD
