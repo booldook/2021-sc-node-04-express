1ee578e3f886816bb5cb7225d8262ec5547c121c
booldook <booldook@gmail.com>
2021-11-07 11:16:22 +0900

    app.js / post-router.js

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
-/*************** Require ****************/
+/******** Global require *******/
 const express = require("express");
 const app = express();
 
-/************** view Init ***************/
-// app.set("view", "ejs")
-app.set("view engine", "pug");
-app.set("views", "./views-pug");
-// app.locals: view template 이 접근 가능한 전역변수객체
-app.locals.pretty = true;
-app.locals.headTitle = "Hello express";
-
-/************* Server Init **************/
+/********* Server Init *********/
 require("./modules/server-init")(app, 3000);
 
-/************* Router Init **************/
-const authRouter = require("./routes/auth-router");
-const userRouter = require("./routes/user-router");
-const boardRouter = require("./routes/board-router");
-const cartRouter = require("./routes/cart-router");
-const pugRouter = require("./routes/pug-router");
-const ejsRouter = require("./routes/ejs-router");
-app.use("/", express.static("./public"));
-app.use("/pug", pugRouter);
-app.use("/ejs", ejsRouter);
-app.use("/auth", authRouter);
-app.use("/user", userRouter);
-app.use("/board", boardRouter);
-app.use("/cart", cartRouter);
+/********* Views Init **********/
+app.set("view engine", "pug");
+app.set("views", "./views-pug");
 
-/* 
-/auth/login(GET)  - 로그인 폼     
-/auth/login(POST) - 로그인     
-/auth/join(GET)   - 회원가입폼
-/auth/join(POST)  - 회원저장
-/user/form(GET)   - 회원수정폼
-/user/form(POST)  - 회원수정
-/board(GET)       - 전체게시글
-/board/1(GET)     - 1번게시글
-/cart             - 전체 장바구니 
-/cart/5           - 장바구니에 담긴 5번 상품 
-*/
+/********* Router Init *********/
+const postRouter = require("./routes/post-router");
+
+app.use("/", express.static("./public"));
+app.use("/post", postRouter);
