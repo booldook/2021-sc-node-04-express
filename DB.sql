d667b0d992cc0b11e6d55d4d7b29144a47037570
booldook <booldook@gmail.com>
2021-11-14 10:24:18 +0900

    DB.sql

---
+++ DB.sql
@@ -0,0 +1,32 @@
+-- Structured QUERY Language
+CREATE TABLE `board4` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`content` TEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`writer` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=INNODB;
+
+-- Create
+INSERT INTO board SET 
+title='테스트', writer='불뚝', content='내용입니다.';
+
+INSERT INTO board (`title`, `writer`, `content`) 
+VALUES 
+('테스트2', '테스터', '내용2 입니다.');
+
+-- DELETE
+DELETE FROM board WHERE id='1';
+
+
+
+
+
+
+
+
+
