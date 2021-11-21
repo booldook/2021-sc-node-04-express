866011570e0e4a5507ca0b50e42ee2e298bdf653
booldook <booldook@gmail.com>
2021-11-21 15:39:06 +0900

    multer-mw

--- middlewares/multer-mw.js
+++ middlewares/multer-mw.js
@@ -4,6 +4,12 @@ const fs = require("fs-extra");
 const moment = require("moment");
 const { v4: uuidv4 } = require("uuid"); // ES6 -> import { v4 as uuidv4 } from 'uuid';
 
+const allowImg = ["jpg", "jpeg", "png", "gif"];
+const allowDoc = ["txt", "pdf", "xls", "xlsx", "ppt", "pptx", "doc", "docx", "hwp"];
+const allowMovie = ["wmv", "mp3", "mp4", "mov"];
+const allowZip = ["zip", "alz"];
+const allowFile = [...allowImg, ...allowDoc, ...allowZip, ...allowMovie];
+
 const storage = multer.diskStorage({
   destination: async (req, file, cb) => {
     try {
@@ -26,4 +32,18 @@ const storage = multer.diskStorage({
   },
 });
 
-module.exports = multer({ storage });
+const limits = { fileSize: 1024000 * 5, files: 2, fields: 6, parts: 6 };
+
+const fileFilter = (req, file, cb) => {
+  try {
+    let ext = path.extname(file.originalname).substr(1).toLowerCase();
+    let type = file.fieldname === "uploadImg" ? "I" : "F";
+    if (type === "I" && allowImg.includes(ext)) cb(null, true);
+    else if (type === "F" && allowFile.includes(ext)) cb(null, true);
+    else cb(new Error("업로드 할 수 없는 파일입니다."));
+  } catch (err) {
+    cb(err);
+  }
+};
+
+module.exports = multer({ storage, limits, fileFilter });
